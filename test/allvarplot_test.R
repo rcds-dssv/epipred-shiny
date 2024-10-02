772c7f7bd9465a1f89f620743df74997f8fdc670
John Lee <swannyy.stat@gmail.com>
2024-10-02 13:34:56 -0500

    Delete contents from test folder

--- test/allvarplot_test.R
+++
@@ -1,9 +0,0 @@
-source("R/1-setup.R")
-source("R/3-utils.R")
-source("R/4-slider_vis.R")
-source("R/5-singleVarPlot.R")
-
-mutations <- read.csv(file.path("data","STXBP1.csv"))
-mutations <- clean_mutations(mutations)
-
-
